6fa9825adb4ec87cc5646c7e88876b5075b463b6
rebecca-pete <rebecca@rebeccapeterson.us>
2019-05-13 22:38:29 -0700

    math and greet with multiply and divide tests

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -12,4 +12,25 @@ describe('proof of life', () => {
     let test = arithmetic.subtract(1, 3);
     expect(test).toBeTruthy();
   });
+
+  it('this multiplies a times b to return 21', () => {
+    let test = arithmetic.multiply(3, 7);
+    expect(test).toBeTruthy();
+  });
+});
+
+describe('proof of life', () => {
+  it('this divides the first value by the second', () => {
+    let test = arithmetic.divide(10, 5);
+    expect(test).toBeTruthy();
+  });
+});
+
+describe('proof of life', () => {
+  it('this takes in an array and integer and multiplies each element in the array by the integer', () => {
+    let arr = [-2, 4, 6, 8];
+    let b = 4;
+    let test = arithmetic.multiplyInArr(arr, b);
+    expect(test).toBeTruthy();
+  });
 });
--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -4,7 +4,7 @@ const greet = require('../lib/greet.js');
 
 describe('proof of life', () => {
   let test = greet.sayHello('John');
-  it('return the user name when the name provided is a string', () => {
+  it(`return a greeting to the user John when the name provided is a string, for example, 'hello John'`, () => {
     expect(test).toBeTruthy();
   });
 });
